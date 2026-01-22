59f026fbef00297d412a29396b88ce17d57cb4af
dnda <dndamc@gmail.com>
2026-01-22 00:42:21 -0500

    added index in memory using a hash (dictionary)

--- engine.py
+++ engine.py
@@ -1,9 +1,40 @@
+from pathlib import Path
+
 from storage import Storage
 
 
-storage = Storage("data/log.dat")
+class Engine:
+    def __init__ (self):
+        self.storage = Storage("data/log.dat")
+        self.index = {} #for a while it is 
+        #self.path_index = Path("data/index.dat")
+        #self.path_index.parent.mkdir(parents=True, exist_ok=True)
+    
 
-storage.read_at(storage.add('clave1','valor1'))
-storage.read_at(storage.add('clave2','valor2'))
-
-
+    def save_index(self,offset,key):
+        self.index[key] = offset
+        
+        return
+        #with open(self.path_index,'a') as f:
+        #    f.write(key+" "+str(offset))
+        
+    def store_pair(self, key,value):
+        self.save_index(self.storage.add(key,value),key) # the index dont save values, only pointers
+        #self.save_index(self.storage.add('clave1','valor1'),key)
+        #self.save_index(self.storage.add('clave2','valor2'),value)
+        
+    def read_by_offset(self,offset):
+        self.storage.read_at(offset)
+    
+    def read_from_index(self,key):
+        if key not in self.index:
+            return None
+        self.read_by_offset(self.index[key])
+        
+        
+        
+    
+if __name__ == "__main__":
+    engine = Engine()
+    engine.store_pair("usuario", "contraseña")
+    engine.read_from_index("usuario")
